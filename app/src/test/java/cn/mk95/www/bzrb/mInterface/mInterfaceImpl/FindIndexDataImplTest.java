6ee0bb9013f8f7a4493ac04ad64dc94b1e8a9c60
yangyonghao <1114951496@qq.com>
2017-06-07 18:56:24 +0800

    最新

---
+++ app/src/test/java/cn/mk95/www/bzrb/mInterface/mInterfaceImpl/FindIndexDataImplTest.java
@@ -0,0 +1,16 @@
+package cn.mk95.www.bzrb.mInterface.mInterfaceImpl;
+
+import org.junit.Test;
+
+
+/**
+ * Created by 11149 on 2017/6/7.
+ */
+public class FindIndexDataImplTest {
+    @Test
+    public void findIndexPageOneData() {
+        FindIndexDataImpl findIndexData=new FindIndexDataImpl();
+        findIndexData.findIndexPageOndeData();
+    }
+
+}
